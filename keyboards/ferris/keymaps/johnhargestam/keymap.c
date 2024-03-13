57f2e53e90804291e8f40bee83326489ce69dd92
unknown <john.hargestam@gmail.com>
2024-03-13 15:24:32 +0100

    add window swap macros (windows)

--- keyboards/ferris/keymaps/johnhargestam/keymap.c
+++ keyboards/ferris/keymaps/johnhargestam/keymap.c
@@ -2,35 +2,44 @@
 #include "sendstring_swedish.h"
 
 #define _____ KC_NO
+#define FN_NWIN A(KC_ESC)
+#define FN_AWIN A(KC_TAB)
+#define FN_PWIN S(A(KC_ESC))
+
+enum layers {
+  DEF
+  SYM
+  FUN
+}
 
 enum keycodes {
-    // Undead versions of dead keys
-    UD_CIRC = SAFE_RANGE,
-    UD_GRV,
-    UD_TILD,
-    // Macros
-    MC_SCOP,
+  // Undead versions of dead keys
+  UD_CIRC = SAFE_RANGE,
+  UD_GRV,
+  UD_TILD,
+  // Macros
+  MC_SCOP,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_split_3x5_2(
-    SE_Q,    SE_W,    SE_F,    SE_P,    SE_B,    SE_J,    SE_L,     SE_U,    SE_Y,    _____,
-    SE_A,    SE_R,    SE_S,    SE_T,    SE_G,    SE_M,    SE_N,     SE_E,    SE_I,    SE_O,
-    SE_Z,    SE_X,    SE_C,    SE_D,    SE_V,    SE_K,    SE_H,     SE_COMM, SE_DOT,  SE_PLUS,
+	[DEF] = LAYOUT_split_3x5_2(
+    SE_Q,    SE_W,    SE_F,    SE_P,    SE_B,    SE_J,    SE_L,    SE_U,    SE_Y,    _____,
+    SE_A,    SE_R,    SE_S,    SE_T,    SE_G,    SE_M,    SE_N,    SE_E,    SE_I,    SE_O,
+    SE_Z,    SE_X,    SE_C,    SE_D,    SE_V,    SE_K,    SE_H,    SE_COMM, SE_DOT,  SE_PLUS,
                                OSL(1),  KC_LSFT, KC_SPC,  OSL(2)
   ),
-	[1] = LAYOUT_split_3x5_2(
+	[SYM] = LAYOUT_split_3x5_2(
     SE_PERC, SE_SLSH, SE_QUOT, SE_LBRC, SE_DLR,  UD_GRV,  SE_RBRC, SE_DQUO, SE_BSLS, UD_TILD,
     SE_ARNG, SE_MINS, SE_EQL,  SE_LPRN, SE_LABK, SE_RABK, SE_RPRN, SE_ADIA, SE_UNDS, SE_ODIA,
     SE_HASH, SE_ASTR, UD_CIRC, SE_LCBR, SE_AT,   MC_SCOP, SE_RCBR, SE_PIPE, SE_AMPR, SE_EXLM,
                                KC_TRNS, KC_TRNS, KC_TRNS, OSL(2)
   ),
-	[2] = LAYOUT_split_3x5_2(
-    _____,  SE_7,    SE_8,    SE_9,     _____,   KC_DOWN, KC_RGHT, KC_DEL,  KC_LGUI, _____,
-    SE_0,   SE_1,    SE_2,    SE_3,     _____,   KC_TAB,  KC_ESC,  KC_BSPC, KC_LCTL, CW_TOGG,
-    _____,  SE_4,    SE_5,    SE_6,     _____,   KC_UP,   KC_LEFT, KC_ENT,  KC_LALT, _____,
-                              OSL(1),   KC_TRNS, KC_TRNS, KC_TRNS
-    ),
+	[FUN] = LAYOUT_split_3x5_2(
+    _____,   SE_7,    SE_8,    SE_9,    FN_NWIN, KC_DOWN, KC_RGHT, KC_DEL,  KC_LGUI, _____,
+    SE_0,    SE_1,    SE_2,    SE_3,    FN_AWIN, KC_TAB,  KC_ESC,  KC_BSPC, KC_LCTL, CW_TOGG,
+    _____,   SE_4,    SE_5,    SE_6,    FN_PWIN, KC_UP,   KC_LEFT, KC_ENT,  KC_LALT, _____,
+                               OSL(1),  KC_TRNS, KC_TRNS, KC_TRNS
+  ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
